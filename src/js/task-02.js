6d88a6f4d8ef2f3ec5c4db39d531760cdc58dbb6
LeraF825 <fertikovalera@gmail.com>
2023-04-23 21:40:16 +0300

    task-2

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,12 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsItem = document.querySelector("#ingredients")
+const newIngredients = ingredients.map(ingredient =>{
+  const elemItem = document.createElement('li');
+  elemItem.textContent = ingredient;
+  elemItem.classList.add('item');
+  return elemItem
+})
+ingredientsItem.append(...newIngredients)
