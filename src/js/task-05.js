76354a74521309aa23795347d4f35c632e0410da
LeraF825 <fertikovalera@gmail.com>
2023-04-24 14:35:20 +0300

    task-5

---
+++ src/js/task-05.js
@@ -0,0 +1,11 @@
+const inputName = document.querySelector('#name-input');
+const outputName = document.querySelector('#name-output');
+
+inputName.addEventListener('input', onInputChange);
+function onInputChange (event){
+    if(inputName.value === '') {
+        outputName.textContent = 'Anonymous'
+    } else {
+        outputName.textContent = event.currentTarget.value;
+    }
+}
